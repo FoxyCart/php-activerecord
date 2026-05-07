c8859b8c8aa43a95222c8ee79febdc2ce8002c12
filipcujanovic <29860740+filipcujanovic@users.noreply.github.com>
2026-05-07 20:03:40 +0200

    fix(Validations): update method signature

--- lib/Validations.php
+++ lib/Validations.php
@@ -884,7 +884,7 @@ class Errors implements IteratorAggregate
 	 *
 	 * @return ArrayIterator
 	 */
-	public function getIterator()
+	public function getIterator(): ArrayIterator
 	{
 		return new ArrayIterator($this->full_messages());
 	}
